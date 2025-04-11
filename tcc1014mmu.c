7d44d2d0470b7b8bc4179bdcabbea31549494638
Craig Allsop <>
2025-04-11 17:45:29 +1000

    Prevent coco3.rom being loaded from the root directory

--- tcc1014mmu.c
+++ tcc1014mmu.c
@@ -212,12 +212,17 @@ void CopyRom(void)
 
 	GetIniFilePath(IniFilePath);
 	GetPrivateProfileString("DefaultPaths", "COCO3ROMPath", "", COCO3ROMPath, MAX_PATH, IniFilePath);
-	unsigned short temp=0;
-	strcat(COCO3ROMPath, "\\coco3.rom");
-	if (COCO3ROMPath != "") { temp = load_int_rom(COCO3ROMPath); } //Try loading from the user defined path first.
-	if (temp) { OutputDebugString(" Found coco3.rom in COCO3ROMPath\n"); }
+	unsigned short temp = 0;
+	if (strcmp(COCO3ROMPath, "") != 0)
+	{
+		strcat(COCO3ROMPath, "\\coco3.rom");
+		temp = load_int_rom(COCO3ROMPath); //Try loading from the user defined path first.
+		if (temp) OutputDebugString(" Found coco3.rom in COCO3ROMPath\n");
+	}
 
-	if (temp == 0) { temp = load_int_rom(BasicRomName()); }		//Try to load the image
+	if (temp == 0)
+		temp = load_int_rom(BasicRomName()); //Try to load the image
+
 	if (temp == 0)
 	{	// If we can't find it use default copy
 		GetModuleFileName(NULL, ExecPath, MAX_PATH);
@@ -225,6 +230,7 @@ void CopyRom(void)
 		strcat(ExecPath, "coco3.rom");
 		temp = load_int_rom(ExecPath);
 	}
+
 	if (temp == 0)
 	{
 		MessageBox(0, "Missing file coco3.rom", "Error", 0);
